336277258c0229339810a815568ed0eddc93ce11
MoeenQadir <myselfxdeveloper@gmail.com>
2024-05-14 21:13:47 +0500

    fix

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -5,7 +5,7 @@ const PrivacyPolicy = () => {
     return (
         <>
             <NavbarBar/>
-            <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8" >
+            <div className="min-h-screen bg-white py-12 px-4 sm:px-6 lg:px-8" >
                 <div className="max-w-4xl mx-auto">
                     <h1 className="text-4xl font-bold text-gray-800 mb-8">Privacy Policy</h1>
                     <p className="text-lg text-gray-700 mb-6">
--- src/pages/TermsServices.jsx
+++ src/pages/TermsServices.jsx
@@ -5,7 +5,7 @@ const TermsService = () => {
     return (
         <>
             <NavbarBar/>
-            <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+            <div className="min-h-screen bg-white py-12 px-4 sm:px-6 lg:px-8">
                 <div className="max-w-4xl mx-auto">
                     <h1 className="text-4xl font-bold text-gray-800 mb-8">Terms of Service</h1>
                     <div className="mb-8">
