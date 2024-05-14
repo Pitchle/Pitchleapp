0ca82174a72f9cfe97dd149f17ac4070fbb92b26
MoeenQadir <myselfxdeveloper@gmail.com>
2024-05-14 23:00:42 +0500

    lun tn charh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,9 @@ const Navbar = () => {
         setIsDrawerOpen(false);
     };
 
-
+    const scrollToTop = () => {
+        window.scrollTo(0, 0);
+    };
     return (
         <>
             <div className={"w-full sticky top-0 z-30 flex justify-center items-center text-white"} >
@@ -20,11 +22,11 @@ const Navbar = () => {
                         <li className="relative group">
                             <div className="c-underline text-2xl font-bold cursor-pointer">Company</div>
                             <ul className="absolute left-[0.2] mt-1 w-48 bg-white shadow-md rounded-md p-2 space-y-2 hidden group-hover:block">
-                                <li className="text-black cursor-pointer"><Link to={"/about"}>About Us</Link></li>
-                                <li className="text-black cursor-pointer"><Link to={"/faq"}>FAQs</Link></li>
+                                <li className="text-black cursor-pointer"><Link onClick={scrollToTop} to={"/about"}>About Us</Link></li>
+                                <li className="text-black cursor-pointer"><Link onClick={scrollToTop} to={"/faq"}>FAQs</Link></li>
                             </ul>
                         </li>
-                        <li className="c-underline text-2xl font-bold cursor-pointer"><Link to={"/plans"}>Plans</Link></li>
+                        <li className="c-underline text-2xl font-bold cursor-pointer"><Link onClick={scrollToTop} to={"/plans"}>Plans</Link></li>
                     </ul>
                     <div className="lg:hidden my-auto font-bold"> {/* Show only in mobile view */}
                         <button onClick={toggleDrawer} className="focus:outline-none">
@@ -35,13 +37,13 @@ const Navbar = () => {
                         {/* Mobile view submenu */}
                         {isDrawerOpen && (
                             <ul className="absolute left-0 w-48 bg-white shadow-md rounded-md p-2 space-y-2">
-                                <li className="text-black cursor-pointer"><Link to={"/plans"}>Plans</Link></li>
-                                <li className="text-black cursor-pointer"><Link to={"/about"}>About Us</Link></li>
-                                <li className="text-black cursor-pointer"><Link to={"/faq"}>FAQs</Link></li>
+                                <li className="text-black cursor-pointer"><Link onClick={scrollToTop} to={"/plans"}>Plans</Link></li>
+                                <li className="text-black cursor-pointer"><Link onClick={scrollToTop} to={"/about"}>About Us</Link></li>
+                                <li className="text-black cursor-pointer"><Link onClick={scrollToTop} to={"/faq"}>FAQs</Link></li>
                             </ul>
                         )}
                     </div>
-                    <div className={"text-center my-auto text-4xl md:text-5xl -ms-4 md:ms-0 font-bold cursor-pointer"}> <Link to={"/"}> Pitchle </Link></div>
+                    <div className={"text-center my-auto text-4xl md:text-5xl -ms-4 md:ms-0 font-bold cursor-pointer"}> <Link onClick={scrollToTop} to={"/"}> Pitchle </Link></div>
                     <div className={"flex justify-end"}><img src="/img/logo/logo.png" className={"md:w-16 rotate-image md:h-16 h-12 w-12"} alt="logo"/></div>
                 </div>
             </div>
